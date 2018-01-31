ad1aca0a1b862750cd18523cf7fe963ae3cb2b96
cjn <1711811966@qq.com>
2018-01-31 17:17:42 +0800

    laravel

---
+++ app/Http/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// 后台模块
+Route::group(['prefix' => 'admin'],function(){
+	Route::get('/',function(){
+		return view('admin.layout.index');
+	});
+	// 后台用户模块
+	Route::resource('user','admin\UserController');
+	Route::get('user/{id}/info','admin\UserController@info');
+	// 后台管理员模块
+	Route::resource('administrator','admin\AdministratorController');
+	// 后台店铺模块
+	Route::resource('shopcate','admin\ShopcateController');
+});
